2ea999d9d62c70a57afd2dbd7baa6b1242f580b9
Ayushi <ayushi.gupta.038@gmail.com>
2024-01-13 00:30:25 +0530

    Add more features

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const movingPlate = document.getElementById('aarti1');
+const centerX = window.innerWidth / 2;
+const centerY = window.innerHeight / 2;
+const radius = 200;
+let angle = 0;
+var dropdown = document.getElementById("myDropdown");
+
+
 function showImage(){
-    var dropdown = document.getElementById("myDropdown");
+
+    
     var selectedOption = dropdown.options[dropdown.selectedIndex].value;
     var image = document.getElementById("imageDisplay");
 
@@ -11,8 +20,8 @@ function showImage(){
         image.src = "./images/ram-sita.webp";
     else if(selectedOption == "ganesha")
         image.src = "./images/ganesha.png";
-    else if(selectedOption == "saraswati")
-        image.src = "./images/saraswati.jpg";
+    else if(selectedOption == "lakshmi")
+        image.src = "./images/lakshmi-devi.jpg";
     else if(selectedOption == "bajrang-bali")
         image.src = "./images/bajrang-bali.webp";
     else
@@ -24,21 +33,52 @@ function pray(){
     document.getElementById('pray1').style.display = "initial";
     document.getElementById('pray2').style.display = "initial";
     document.getElementById('aarti1').style.display = "none";
-    document.getElementById('aarti2').style.display = "none";
 }
 
 function aarti(){
+    var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+
     document.getElementById('pray1').style.display = "none";
     document.getElementById('pray2').style.display = "none";
     document.getElementById('aarti1').style.display = "initial";
-    document.getElementById('aarti2').style.display = "initial";
+
+    movePlate();
+
+    var audio;
+
+    if(selectedOption == "krishna")
+        audio = new Audio('./audios/krishna-aarti.mp3');
+    else if(selectedOption == "mahadev")
+        audio = new Audio('./audios/shivji-aarti.mp3');
+    else if(selectedOption == "ram")
+        audio = new Audio('./audios/ram-aarti.mp3');
+    else if(selectedOption == "ganesha")
+        audio = new Audio('./audios/ganesh-aarti.mp3');
+    else if(selectedOption == "lakshmi")
+        audio = new Audio('./audios/lakshmi-aarti.mp3');
+    else if(selectedOption == "bajrang-bali")
+        audio = new Audio('./audios/hanuman-aarti.mp3');
+    else
+        audio = new Audio('');
+
+    audio.play();
+}
+
+function movePlate() {
+    const x = centerX + radius*Math.cos(angle);
+    const y = centerY + radius*Math.sin(angle);
+
+    movingPlate.style.left = x + 'px';
+    movingPlate.style.top = y + 'px';
+
+    angle += 0.01;
+    requestAnimationFrame(movePlate);
 }
 
 function meditate(){
     document.getElementById('pray1').style.display = "none";
     document.getElementById('pray2').style.display = "none";
     document.getElementById('aarti1').style.display = "none";
-    document.getElementById('aarti2').style.display = "none";
 
     var audio = new Audio('./audios/meditation.mp3');
     audio.play();
@@ -48,7 +88,7 @@ function donate(){
     document.getElementById('pray1').style.display = "none";
     document.getElementById('pray2').style.display = "none";
     document.getElementById('aarti1').style.display = "none";
-    document.getElementById('aarti2').style.display = "none";
 
     alert("Not taking Donations currently. Thank you for consideration!")
 }
+
